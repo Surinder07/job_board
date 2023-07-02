3e33bd45a70845b2fbf00deb68b46fdd941968ba
Soham Chaudhary <sohamamrit@gmail.com>
2023-07-02 14:21:39 -0400

    React

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,9 @@
 import logo from './logo.svg';
-import React, { useEffect, useState } from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import {List} from "reactstrap";
 
-
+/*
 const App = () => {
 
   const [employers, setEmployers] = useState([]);
@@ -34,6 +35,42 @@ const App = () => {
         </header>
       </div>
   );
+}*/
+
+class App extends Component {
+    state = {
+        clients: []
+    };
+
+    async componentDidMount() {
+        const response = await fetch('/employers');
+        const body = await response.json();
+        this.setState({clients: body});
+/*        client({method: 'GET', path: '/employers'}).done(response => {
+            this.setState({employees: response.entity.employees});
+        });*/
+    }
+
+    render() {
+        const {clients} = this.state;
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <div className="App-intro">
+                        <h2>Employees</h2>
+                       {clients.map(client =>
+                            <div key={client.id}>
+                                {client.password} ({client.email})
+                            </div>
+                        )}
+
+                    </div>
+                </header>
+            </div>
+        );
+    }
 }
+
 
 export default App;
